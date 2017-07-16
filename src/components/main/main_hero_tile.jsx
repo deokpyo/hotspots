90158db273d04b69822f3db8ee26d886ff339a41
SONIK JHANG <sonik.jhang@utexas.edu>
2017-07-16 01:43:21 -0500

    main page

---
+++ src/components/main/main_hero_tile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const MainHeroTile = ({ image='' }) => {
+  return (
+    <div className="w-clearfix w-col w-col-6 w-dyn-item">
+      <div className="_50 _60block" data-ix="tile-1">
+        <a className="hero-tile special w-inline-block" href="/about">
+          <div className={`tile-image ${image}`}>
+            <div className="tile-overlay">
+              <div className="project-tag"></div>
+              <div className="spot-name tile-text"></div>
+              <div className="details">
+                <div className="bed"></div>
+                <div className="divider"></div>
+                <div className="location"></div>
+                <div className="divider"></div>
+                <div className="description"></div>
+              </div>
+              <div className="hotdeal">
+                <div className="deal"></div>
+                <div className="price"></div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default MainHeroTile;
