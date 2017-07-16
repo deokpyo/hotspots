f4c10027e3a478e4a78c2437419998794fbff912
Jimmy Hong <jimmyhong.rocks@gmail.com>
2017-07-15 22:41:25 -0500

    updated image and hide public for development

--- src/components/landing/landing_page.jsx
+++ src/components/landing/landing_page.jsx
@@ -13,7 +13,7 @@ class LandingPage extends React.Component {
                 <div className="w-clearfix">
                   <HeroTile size='60' text='Trending' ix='1' image='trending'/>
                   <HeroTile size='40' text='Latest Deals' ix='3' image='latest'/>
-                  <HeroTile size='33' text='Fresh Finds' ix='3'/>
+                  <HeroTile size='33' text='Fresh Finds' ix='3'image='phone'/>
                   <HeroTile size='33' text='Beat the heat' ix='2' />
                   <HeroTile size='33' text='See all' ix='2' image='seeall'/>
                 </div>
